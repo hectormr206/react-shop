50be71585848f29781361f0d11772ea74d7b7a16
hectormr206 <hectormartinezresendiz@gmail.com>
2022-03-30 12:39:02 -0600

    [FIX] Routes

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from '@containers/Layout';
 import Home from '@pages/Home';
 import Login from '@pages/Login';
@@ -21,18 +21,18 @@ const App = () => {
 		<AppContext.Provider value={initialState}>
 			<BrowserRouter>
 				<Layout>
-					<Switch>
-						<Route exact path="/" component={Home} />
-						<Route exact path="/login" component={Login} />
-						<Route exact path="/password-recovery" component={PasswordRecovery} />
-						<Route exact path="/send-email" component={SendEmail} />
-						<Route exact path="/new-password" component={NewPassword} />
-						<Route exact path="/account" component={MyAccount} />
-						<Route exact path="/signup" component={CreateAccount} />
-						<Route exact path="/checkout" component={Checkout} />
-						<Route exact path="/orders" component={Orders} />
-						<Route path="*" component={NotFound} />
-					</Switch>
+					<Routes>
+						<Route exact path="/" element={<Home />} />
+						<Route exact path="/login" element={<Login />} />
+						<Route exact path="/password-recovery" element={<PasswordRecovery />} />
+						<Route exact path="/send-email" element={<SendEmail />} />
+						<Route exact path="/new-password" element={<NewPassword />} />
+						<Route exact path="/account" element={<MyAccount />} />
+						<Route exact path="/signup" element={<CreateAccount />} />
+						<Route exact path="/checkout" element={<Checkout />} />
+						<Route exact path="/orders" element={<Orders />} />
+						<Route path="*" element={<NotFound />} />
+					</Routes>
 				</Layout>
 			</BrowserRouter>
 		</AppContext.Provider>
